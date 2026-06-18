fdbf56bbcf23069045dd03b30cd51e18977c9290
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-18 01:43:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: process.env.VITE_INDEX || 'index.html'
+    }
+  }
 })
