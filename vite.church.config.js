a6228fae8a59f9146bc5f359bf5b64f19fa7369b
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-21 19:33:06 +0100

    Update vite.church.config.js

--- vite.church.config.js
+++ vite.church.config.js
@@ -1,1 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'church-index.html'
+    },
+    outDir: 'dist'
+  }
+})
