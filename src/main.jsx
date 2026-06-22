2476ebb8127b28d424bbf33c9b8b320e8659ab4f
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-22 06:32:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ChurchApp from './ChurchApp.jsx'
+
+const mode = localStorage.getItem('tracka_mode');
+const AppToLoad = mode === 'church' ? ChurchApp : App;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppToLoad/>
   </React.StrictMode>
 )
