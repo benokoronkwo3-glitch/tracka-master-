771136447afcd8c4d10e2fe878e1dc184533cec3
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-23 05:15:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import ChurchApp from './ChurchApp.jsx'
-
-const p = new URLSearchParams(window.location.search);
-const churchId = p.get('church');
-const stored = localStorage.getItem('tracka_church_client');
 const isChurch = !!(churchId || stored);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {isChurch ? <ChurchApp/> : <App/>}
-  </React.StrictMode>
-)
+{isChurch ? <ChurchApp/> : <App/>}
