ebdeea9a9d20680a787850f7d0c0016d53690458
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-23 23:05:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ChurchApp from './ChurchApp.jsx'
 
-const p = new URLSearchParams(window.location.search)
-const churchId = p.get('church')
-const stored = localStorage.getItem('tracka_church_client')
-const isChurch = !!(churchId || stored)
+function Root() {
+  const params = new URLSearchParams(window.location.search)
+  const churchId = params.get('church')
+  const storedChurch = localStorage.getItem('tracka_church_client')
+
+  if (churchId) {
+    localStorage.setItem('tracka_church_client', churchId)
+    return <ChurchApp />
+  }
+
+  if (storedChurch) {
+    return <ChurchApp />
+  }
+
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isChurch ? <ChurchApp /> : <App />}
+    <Root />
   </React.StrictMode>
 )
