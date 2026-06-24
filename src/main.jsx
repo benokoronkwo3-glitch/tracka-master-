3f995605f2386a0601d4882f05f3678c9dc395e6
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-24 09:26:01 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import ChurchApp from './ChurchApp.jsx'
+
+function Root() {
+  const params = new URLSearchParams(window.location.search)
+  const churchId = params.get('church')
+  const storedChurch = localStorage.getItem('tracka_church_client')
+  if (churchId) {
+    localStorage.setItem('tracka_church_client', churchId)
+    return <ChurchApp />
+  }
+  if (storedChurch) {
+    return <ChurchApp />
+  }
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div style={{padding:40,fontSize:24,fontWeight:800,color:"green"}}>
-      TRACKA CHURCH IS WORKING
-    </div>
-  </React.StrictMode>
+  <React.StrictMode><Root /></React.StrictMode>
 )
